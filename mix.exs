f12ed39a1921aeba74c0a19d423da0a672b077cc
vitorlimadev <vitorlimadev@outlook.com>
2023-09-08 17:44:50 +0300

    chore: add Absinthe

--- mix.exs
+++ mix.exs
@@ -32,7 +32,9 @@ defmodule Arvore.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:absinthe, "~> 1.7.0"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
